c6e490882dd982a6a7d1a177daf16a1bf33639d4
Joseph Yeh <jyeh@mozilla.com>
2016-09-23 15:26:36 +0800

    Use enzyme for testing React components

--- src/components/NavLink.test.js
+++ src/components/NavLink.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import NavLink from './NavLink';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NavLink />, div);
+describe('<NavLink />', () => {
+  it('renders a <Link />', () => {
+    const wrapper = shallow(<NavLink />);
+    expect(wrapper.is('Link')).toBe(true);
+  });
 });
